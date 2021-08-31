d22f81dff6ed01231fa2829419edd0f497d989a5
Maros Kutschy <maros.kutschy@masternaut.com>
2021-08-31 15:26:45 +0200

    updated files + added new files

---
+++ src/com/ness/Customer.java
@@ -0,0 +1,14 @@
+package com.ness;
+
+public class Customer {
+
+    private String name;
+    private double balance;
+
+    public Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+
+}
