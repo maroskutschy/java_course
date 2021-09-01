763ed0402ac74c1b255873af8689b51d4e4771c2
Maros Kutschy <maroskutschy@gmail.com>
2021-09-01 07:14:18 +0200

    adjusted files

--- src/com/ness/Customer.java
+++ src/com/ness/Customer.java
@@ -10,5 +10,19 @@ public class Customer {
         this.balance = balance;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
---
+++ src/com/ness/Main5.java
@@ -0,0 +1,32 @@
+package com.ness;
+
+import java.util.ArrayList;
+
+public class Main5 {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer("Maros", 100.36);
+        Customer anotherCustomer = customer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.69);
+        System.out.println("Balance for cumtomer " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for (int i=0; i<intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1,2);
+
+        for (int i=0; i<intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+
+    }
+}
